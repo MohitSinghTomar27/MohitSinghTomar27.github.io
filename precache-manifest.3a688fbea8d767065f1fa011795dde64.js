6a41babf28cfe2fba44a6481f8964069ee003e88
Mohit2708 <t-mohit.singh.tomar@heart.org>
2023-05-12 10:42:10 +0530

    initial commit

---
+++ precache-manifest.3a688fbea8d767065f1fa011795dde64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bf6435ff789719085faf1e570a3f93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63064798e3434add7fbc",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cf4575457d124a787382",
+    "url": "/static/js/2.36f73b32.chunk.js"
+  },
+  {
+    "revision": "63064798e3434add7fbc",
+    "url": "/static/js/main.13b52eb4.chunk.js"
+  },
+  {
+    "revision": "9b5777feb30772fec0e3",
+    "url": "/static/js/runtime-main.1dd3c951.js"
+  }
+]);
